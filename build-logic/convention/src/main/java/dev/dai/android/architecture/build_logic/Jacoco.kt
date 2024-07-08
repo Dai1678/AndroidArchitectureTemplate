13e1866976ee8b01f0c58cfee3889fa5471e3502
Dai1678 <dai7774@gmail.com>
2024-07-08 20:29:13 +0900

    fix outputLocal

--- build-logic/convention/src/main/java/dev/dai/android/architecture/build_logic/Jacoco.kt
+++ build-logic/convention/src/main/java/dev/dai/android/architecture/build_logic/Jacoco.kt
@@ -83,8 +83,12 @@ internal fun Project.configureJacoco(
           .matching { exclude(coverageExclusions) },
       )
       reports {
-        xml.required.set(true)
-        xml.outputLocation.set(file("$buildDir/reports/jacoco/merge.xml"))
+        xml.apply {
+          required.set(true)
+          outputLocation.set(
+            file("$buildDir/reports/jacoco/create${variant.name.capitalize()}CombinedCoverageReport/merge.xml")
+          )
+        }
         html.required.set(true)
       }
 

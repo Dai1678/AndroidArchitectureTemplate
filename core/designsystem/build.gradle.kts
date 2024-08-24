5516e5b33c32f3289a486aea83409802cfcf607e
Dai1678 <dai7774@gmail.com>
2024-08-24 19:17:00 +0900

    create designsystem module

---
+++ core/designsystem/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+  alias(libs.plugins.android.architecture.template.android.library)
+  alias(libs.plugins.android.architecture.template.android.library.compose)
+}
+
+android {
+  namespace = "dev.dai.android.architecture.designsystem"
+}

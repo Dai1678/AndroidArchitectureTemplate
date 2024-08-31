8dd1abc7746864109d528b8af81df5e94699f69a
Dai1678 <dai7774@gmail.com>
2024-09-01 01:36:28 +0900

    add refresh test

--- core/test/src/main/java/dev/dai/android/architecture/core/test/repository/FakeUserRepository.kt
+++ core/test/src/main/java/dev/dai/android/architecture/core/test/repository/FakeUserRepository.kt
@@ -2,6 +2,7 @@ package dev.dai.android.architecture.core.test.repository
 
 import dev.dai.android.architecture.core.data.repository.UserRepository
 import dev.dai.android.architecture.core.model.User
+import dev.dai.android.architecture.core.model.fake
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 
@@ -11,7 +12,9 @@ class FakeUserRepository : UserRepository {
 
   override fun users(): Flow<List<User>> = _users
 
-  override suspend fun refresh() = Unit
+  override suspend fun refresh() {
+    _users.emit(listOf(User.fake()))
+  }
 
   suspend fun emitUser(user: List<User>) {
     _users.emit(user)

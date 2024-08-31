8a51a657dc5db79f23b841595ad3c1bb4d1108ce
Dai1678 <dai7774@gmail.com>
2024-08-31 15:14:20 +0900

    implement timber

--- app/src/main/java/dev/dai/android/architecture/template/App.kt
+++ app/src/main/java/dev/dai/android/architecture/template/App.kt
@@ -2,6 +2,18 @@ package dev.dai.android.architecture.template
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+  override fun onCreate() {
+    super.onCreate()
+    setupTimber()
+  }
+}
+
+private fun setupTimber() {
+  if (BuildConfig.DEBUG) {
+    Timber.plant(Timber.DebugTree())
+  }
+}

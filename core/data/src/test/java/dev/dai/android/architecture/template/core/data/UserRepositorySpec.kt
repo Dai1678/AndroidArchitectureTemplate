5c87fccd67e6aee86afdd0c3be6180e5a6dab6af
Dai1678 <dai7774@gmail.com>
2024-10-13 23:57:12 +0900

    Implemented UserRepositorySpec

--- core/data/src/test/java/dev/dai/android/architecture/template/core/data/UserRepositorySpec.kt
+++ core/data/src/test/java/dev/dai/android/architecture/template/core/data/UserRepositorySpec.kt
@@ -4,28 +4,24 @@ import dev.dai.android.architecture.core.data.repository.DefaultUserRepository
 import dev.dai.android.architecture.template.core.model.User
 import dev.dai.android.architecture.template.core.model.fake
 import dev.dai.android.architecture.template.core.testing.datasource.FakeUserNetworkDataSource
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.test.runTest
-import org.junit.Before
-import org.junit.Test
-import kotlin.test.assertEquals
 
-class UserRepositoryTest {
+class UserRepositorySpec : DescribeSpec({
 
-  private lateinit var userRepository: DefaultUserRepository
+  lateinit var userRepository: DefaultUserRepository
 
-  private val userNetworkDataSource = FakeUserNetworkDataSource()
+  val userNetworkDataSource = FakeUserNetworkDataSource()
 
-  @Before
-  fun setup() {
+  beforeSpec {
     userRepository = DefaultUserRepository(userNetworkDataSource)
   }
 
-  @Test
-  fun `users should return a list of users`() = runTest {
+  describe("users") {
     val users = userRepository.users().first()
-    assertEquals(
-      listOf(
+    it("should return a list of users") {
+      users shouldBe listOf(
         User.fake(),
         User.fake(
           id = 2,
@@ -34,8 +30,7 @@ class UserRepositoryTest {
           phone = "1234567890",
           website = "https://dev.dai.com",
         )
-      ),
-      users
-    )
+      )
+    }
   }
-}
+})

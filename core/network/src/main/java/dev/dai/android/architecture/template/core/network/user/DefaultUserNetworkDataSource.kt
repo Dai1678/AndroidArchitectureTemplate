1a5a5a8a6ca9f539b3406aa68a923984e3f208ec
Dai1678 <dai7774@gmail.com>
2024-10-14 12:11:08 +0900

    create users.json

--- core/network/src/main/java/dev/dai/android/architecture/template/core/network/user/DefaultUserNetworkDataSource.kt
+++ core/network/src/main/java/dev/dai/android/architecture/template/core/network/user/DefaultUserNetworkDataSource.kt
@@ -2,49 +2,25 @@ package dev.dai.android.architecture.template.core.network.user
 
 import dev.dai.android.architecture.template.core.network.service.NetworkService
 import dev.dai.android.architecture.template.core.network.user.response.UserResponse
-import dev.dai.android.architecture.template.core.network.user.response.fake
-import kotlinx.coroutines.delay
 import retrofit2.Retrofit
-import java.io.IOException
-import kotlin.random.Random
+import retrofit2.create
+import retrofit2.http.GET
 
-//private interface UserApi {
-//  @GET("/users")
-//  suspend fun getUsers(): List<UserResponse>
-//}
+private interface UserApi {
+  @GET("user/users")
+  suspend fun getUsers(): List<UserResponse>
+}
 
 internal class DefaultUserNetworkDataSource internal constructor(
   private val networkService: NetworkService,
   retrofit: Retrofit,
 ) : UserNetworkDataSource {
 
-  // private val userApi = retrofit.create<UserApi>()
+  private val userApi = retrofit.create<UserApi>()
 
   override suspend fun getUsers(): List<UserResponse> {
     return networkService {
-      delay(SIMULATE_NETWORK_DELAY_MILLIS)
-      // Simulate network success or error
-      if (Random.nextBoolean()) {
-        // userApi.getUsers()
-        buildList {
-          repeat(USER_RESPONSE_SIZE) {
-            UserResponse.fake(
-              id = it,
-              name = "User$it",
-              email = "user$it@dev.dai.com",
-              phone = "1234567890",
-              website = "https://dev.dai.com",
-            ).let { userResponse -> add(userResponse) }
-          }
-        }
-      } else {
-        throw IOException("Network error")
-      }
+      userApi.getUsers()
     }
-  }
-
-  companion object {
-    private const val SIMULATE_NETWORK_DELAY_MILLIS = 1500L
-    private const val USER_RESPONSE_SIZE = 20
   }
 }
